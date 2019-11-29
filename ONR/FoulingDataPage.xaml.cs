83111f57ac4a4ad00b992fdec139558f6e621b3e
Kiley Roberson <kileylroberson@gmail.com>
2019-11-29 12:30:02 -0800

    total macro counter

--- ONR/FoulingDataPage.xaml.cs
+++ ONR/FoulingDataPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -24,6 +25,7 @@ namespace ONR
     public sealed partial class FoulingDataPage : Page
     {
         public FoulingDataEntry data_entry;
+        public float total_macro_count = 0;
         public FoulingDataPage()
         {
             this.InitializeComponent();
@@ -50,6 +52,20 @@ namespace ONR
             this.Frame.Navigate(typeof(BatchHome));
         }
 
+        private void save_and_exit(object sender, RoutedEventArgs e)
+        {
+            /**
+             * TODO: Write contents of textBoxes to database here
+             **/
+             
+            this.Frame.Navigate(typeof(FoulingPanels));
+        }
+
+        private void exit(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(FoulingPanels));
+        }
+
         private void toggle_total_macro(object sender, RoutedEventArgs e)
         {
             ToggleSwitch toggleSwitch = sender as ToggleSwitch;
@@ -67,6 +83,46 @@ namespace ONR
                 }
             }
         }
-        
+
+        private float read_int(TextBox name)
+        {
+            float val = 0;
+            try
+            {
+                string t = name.Text;
+                val  = float.Parse(t, CultureInfo.InvariantCulture);
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse");
+            }
+            return val;
+        }
+
+        private void update_total_macro(object sender, RoutedEventArgs e)
+        {
+
+            float val1 = read_int(barn_perc);
+            float val2 = read_int(moll_perc);
+            float val3 = read_int(psed_perc);
+            float val4 = read_int(pcal_perc);
+            float val5 = read_int(eb_perc);
+            float val6 = read_int(wsp_perc);
+            float val7 = read_int(br_perc);
+            float val8 = read_int(hyd_perc);
+            float val9 = read_int(anen_perc);
+            float val10 = read_int(tun_ct);
+            float val11 = read_int(tun_pcal);
+            float val12 = read_int(amph_cor);
+            float val13 = read_int(sp_perc);
+            float val14 = read_int(alg_pcal);
+            float val15 = read_int(if_field);
+            float val16 = read_int(biofilm_si);
+            this.total_macro_count = val1 + val2 + val3 + val4 + val5 + val6 + val7 + val8 + val9 + val10 + val11 + val12 + val13 + val14 + val15 + val16;
+            string val = (this.total_macro_count).ToString("N2");
+            total_macro.Text = val;
+        }
+
     }
 }
